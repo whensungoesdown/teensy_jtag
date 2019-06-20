7b2f75f98a3914b3a569a4c05f8c49a4461b708c
uty <whensungoes@gmail.com>
2019-06-20 14:46:07 -0400

    fix bug, i mistakly think 1 byte has 4 bits. Until I ouput every bit that send into TDI, I didn't find this.  Get AP[0].IDR corretly without further formating.

--- teensy_jtag.ino
+++ teensy_jtag.ino
@@ -67,10 +67,12 @@ int U_TAPAccessShiftRaw (int numberofbits, byte* pTMSBits,
         if (getbit(pTDIBits, i))
         {
           digitalWrite(PIN_TDI, HIGH);
+          Serial.print("1 ");
         }
         else
         {
           digitalWrite(PIN_TDI, LOW);
+          Serial.print("0 ");
         }
     }
 //    else
@@ -115,7 +117,8 @@ int U_TAPAccessShiftRaw (int numberofbits, byte* pTMSBits,
     digitalWrite(PIN_TCK, LOW);
     delay(5);
   }
-  
+
+  Serial.println("");
   return 0;
 }
 
@@ -132,7 +135,7 @@ int U_TAPAccessShiftDR (int numberofbits, byte* pTDIBits, byte* pTDOBits)
   U_TAPAccessShiftRaw(3, nTMSBits, 0, 0, SHIFTRAW_OPTION_NONE);
 
   // read ID code and leave Shift-DR state with last bit
-  U_TAPAccessShiftRaw(numberofbits, 0, 0, pTDOBits, SHIFTRAW_OPTION_LASTTMS_ONE);
+  U_TAPAccessShiftRaw(numberofbits, 0, pTDIBits, pTDOBits, SHIFTRAW_OPTION_LASTTMS_ONE);
 
   // return to Run-Test/Idle
   nTMSBits[0] = 0x1; // TMS sequence 10
@@ -149,7 +152,7 @@ int U_TAPAccessShiftIR (int numberofbits, byte* pTDIBits, byte* pTDOBits)
   U_TAPAccessShiftRaw(4, nTMSBits, 0, 0, SHIFTRAW_OPTION_NONE);
 
   // send in IR command and leave Shift-IR state with last bit
-  U_TAPAccessShiftRaw(numberofbits, 0, pTDIBits, 0, SHIFTRAW_OPTION_LASTTMS_ONE);
+  U_TAPAccessShiftRaw(numberofbits, 0, pTDIBits, pTDOBits, SHIFTRAW_OPTION_LASTTMS_ONE);
 
   // return to Run-Test/Idle
   nTMSBits[0] = 0x1; // TMS sequence 10
@@ -196,7 +199,7 @@ int GetDAPID2()
 
   U_TAPAccessIdle();
   
-  nTDIBits[0] = 0x0E; // TMS sequence: 1110
+  nTDIBits[0] = 0xE; // IDCODE: 1110
   U_TAPAccessShiftIR(4, nTDIBits, 0);
 
   U_TAPAccessShiftDR(32, 0, nTDOBits);
@@ -206,6 +209,148 @@ int GetDAPID2()
 
   return nIDCode;
 }
+
+int test (void)
+{
+  byte nTDIBits[5] = {0};
+  byte nTDOBits[5] = {0};
+
+  U_TAPAccessIdle();
+  
+  nTDIBits[0] = 0xA; // DPACC: 1010
+  U_TAPAccessShiftIR(4, nTDIBits, 0);
+
+//  // Write 0x50000000 to DP.CTRL/STAT register
+//  // CTRL/STAT A[3:2] 01
+//  // RnW  Write 0
+//  nTDIBits[0] = 0x2;
+//  nTDIBits[1] = 0;
+//  nTDIBits[2] = 0;
+//  nTDIBits[3] = 0x8;
+//  nTDIBits[4] = 0x2;
+//  U_TAPAccessShiftDR(35, nTDIBits, 0);
+//
+//  // Write 0x50000000 to the DP.CTRL/STAT register
+//  // poll the DP.CTRL/STAT register for 0xf0000000
+//  // actually, I got 0x30000000, says CSYSPWRUPREQ and CSYSPWRUPACK unset, 
+//  // CDBGPWRUPREQ and CDBGPWRUPACK set
+//
+//  Serial.println(" > Test write DP CTRL/STAT DATAIN 0x50000000, A[3:2] 01, W");
+//  U_TAPAccessShiftDR(35, 0, nTDOBits);
+//
+//  Serial.print(" > Test read DP CTRL/STAT: ");
+//  for (int i = 4; i >= 0; i--)
+//  {
+//    Serial.print(nTDOBits[i], HEX);
+//  }
+//  Serial.println("");
+
+
+  // write 0x0 to the DP.SELECT register to activatie the AP
+  // at position 0 on the DAP bus
+
+  // DP.SELECT A[3:2] 10
+  // RnW  WRITE 0
+  nTDIBits[0] = 0x84;
+  nTDIBits[1] = 0x07; 
+  nTDIBits[2] = 0x0;
+  nTDIBits[3] = 0x0;
+  nTDIBits[4] = 0x0;
+  U_TAPAccessShiftDR(35, nTDIBits, 0);
+  
+
+  //
+  nTDIBits[0] = 0xB; // APACC: 1011
+  U_TAPAccessShiftIR(4, nTDIBits, 0);
+
+  // A[3:2] 11  the forth register IDR
+  // RnW READ 1
+  nTDIBits[0] = 0x7;
+  nTDIBits[1] = 0x0;
+  nTDIBits[2] = 0x0;
+  nTDIBits[3] = 0x0;
+  nTDIBits[4] = 0x0;
+  U_TAPAccessShiftDR(35, nTDIBits, 0);
+
+  Serial.println(" > Test write DP.SELECT APACC");
+  
+  U_TAPAccessShiftDR(35, 0, nTDOBits);
+
+  Serial.print(" > Test read AP IDR: ");
+  for (int i = 4; i >= 0; i--)
+  {
+    Serial.print(nTDOBits[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println("");
+  
+  return 0;
+}
+
+int dp_rdbuff (void)
+{
+  byte nTDIBits[5];
+  byte nTDOBits[5];
+
+  U_TAPAccessIdle();
+  
+  nTDIBits[0] = 0xA; // DPACC: 1010
+  U_TAPAccessShiftIR(4, nTDIBits, 0);
+
+  //
+  // RDBUFF A[3:2] 11
+  // RnW  READ 1
+  nTDIBits[0] = 0x7;
+  nTDIBits[1] = 0;
+  nTDIBits[2] = 0;
+  nTDIBits[3] = 0x0;
+  nTDIBits[4] = 0x0;
+  U_TAPAccessShiftDR(35, nTDIBits, 0);
+
+  U_TAPAccessShiftDR(35, 0, nTDOBits);
+
+  Serial.println(" > Test read DP.RDBUFF, should be 0");
+  for (int i = 4; i >= 0; i--)
+  {
+    Serial.print(nTDOBits[i], HEX);
+  }
+  Serial.println("");
+  
+  return 0;
+}
+
+int dp_dpidr (void)
+{
+  byte nTDIBits[5];
+  byte nTDOBits[5];
+
+  U_TAPAccessIdle();
+  
+  nTDIBits[0] = 0xA; // DPACC: 1010
+  U_TAPAccessShiftIR(4, nTDIBits, 0);
+
+  //
+  // DPIDR A[3:2] 0
+  // RnW  READ 1
+  nTDIBits[0] = 0x1;
+  nTDIBits[1] = 0;
+  nTDIBits[2] = 0;
+  nTDIBits[3] = 0x0;
+  nTDIBits[4] = 0x0;
+  U_TAPAccessShiftDR(35, nTDIBits, 0);
+
+  U_TAPAccessShiftDR(35, 0, nTDOBits);
+
+  Serial.println(" > Test read DP.DPIDR");
+  for (int i = 4; i >= 0; i--)
+  {
+    Serial.print(nTDOBits[i], HEX);
+  }
+  Serial.println("");
+  
+  return 0;
+}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -237,7 +382,19 @@ void loop() {
       nIDCode = GetDAPID2();
       Serial.print(" > DAP ID: ");
       Serial.println(nIDCode, HEX);
-    }    
+    }  
+    else if (incoming == "test\n") 
+    { 
+      test();
+    }
+    else if (incoming == "dp_rdbuff\n")
+    {
+        dp_rdbuff();
+    }
+    else if (incoming == "dp_dpidr\n")
+    {
+        dp_dpidr();
+    }
     else 
     {
       //invalid command
